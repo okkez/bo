19b0ccc5fb63ae94a474f88fd429fb3c0ced4134
okkez <okkez000@gmail.com>
2009-11-02 15:23:11 +0900

    add google jsapi and bo.css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def google_jsapi_tag
+    '<script type="text/javascript" src="http://www.google.com/jsapi"></script>'
+  end
+
+  def google_load_tag(name, version)
+    '<script type="text/javascript">google.load("#{name}", "#{version}");</script>'
+  end
 end

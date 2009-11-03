99d4375764ee9253151a8defc92d343ba67c81e0
okkez <okkez000@gmail.com>
2009-11-03 14:11:37 +0900

    change TagList.delimiter to ' '

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -6,7 +6,7 @@ Factory.define :morning, :class => Item do |i|
   i.note  'note'
   i.amount 300
   i.after_create{|item|
-    item.tag_list = "食費, 朝食, 菓子パン"
+    item.tag_list = "食費 朝食 菓子パン"
     item.save
   }
 end
@@ -17,7 +17,7 @@ Factory.define :lunch, :class => Item do |i|
   i.note  'note'
   i.amount 520
   i.after_create{|item|
-    item.tag_list = "食費, 昼食, カツ丼"
+    item.tag_list = "食費 昼食 カツ丼"
     item.save!
   }
 end
@@ -28,7 +28,7 @@ Factory.define :dinner, :class => Item do |i|
   i.note  'note'
   i.amount 950
   i.after_create{|item|
-    item.tag_list = "食費, 夕食, ラーメン"
+    item.tag_list = "食費 夕食 ラーメン"
     item.save
   }
 end

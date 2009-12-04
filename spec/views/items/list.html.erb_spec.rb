65b00ac8c33bd2ae64326d8a0585f04ef03066e3
okkez <okkez000@gmail.com>
2009-12-04 15:13:14 +0900

    fix spec to see green

--- spec/views/items/list.html.erb_spec.rb
+++ spec/views/items/list.html.erb_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 describe "/items/list" do
   before(:each) do
+    assigns[:items] = []
     render 'items/list'
   end
 
   #Delete this example and add some real ones or delete this file
   it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/items/list])
+    response.should have_tag('table')
   end
 end

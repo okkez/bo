28ad8bb39b59e13acaa98108901e12e945c9477b
okkez <okkez000@gmail.com>
2010-01-31 01:47:10 +0900

    add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,11 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password
 
+  before_filter :authentication
+
+  private
+
+  def current_user
+    @login_user if defined?(@login_user)
+  end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,35 @@
 class EventsController < ApplicationController
+
+  before_filter :authentication_required, :except => [:index]
+  before_filter :prepare, :only => [:show, :edit]
+
+  # GET events_path
   def index
+    user = current_user
+    if user
+      @events = user.events(:order => 'spent_on ASC')
+    else
+      @events = []
+    end
+  end
+
+  # GET event_path(:id)
+  def show
+  end
+
+  # GET edit_event_path(:id)
+  def edit
+  end
+
+  # DELETE event_path(:id)
+  def destroy
+    Event.destroy(:conditions => { :id => params[:id], :user_id => current_user.id })
+  end
+
+  private
+
+  def prepare
+    @event = current_user.events.first(:conditions => { :id => params[:id] })
   end
 
 end

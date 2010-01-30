5f3b610e9b8186d28eb2bd7ab4d88d29a21032ce
okkez <okkez000@gmail.com>
2010-01-30 13:14:52 +0900

    remove items/list action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,6 @@
 class ItemsController < ApplicationController
 
   def index
-    redirect_to :action => 'list'
-  end
-
-  def list
     @items = Item.all(:order => 'events.spent_on, items.event_id, items.founds_in nulls last')
   end
 

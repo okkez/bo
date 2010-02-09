94be60ce3945cfcaccba6399952261847a9f0cb3
okkez <okkez000@gmail.com>
2010-02-09 23:36:54 +0900

    add events_controller specs

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,16 +2,90 @@ require 'spec_helper'
 
 describe EventsController do
 
-  #Delete these examples and add some real ones
-  it "should use EventsController" do
-    controller.should be_an_instance_of(EventsController)
+  before do
+    @user = Factory(:test)
   end
 
-
   describe "GET 'index'" do
-    it "should be successful" do
-      get 'index'
-      response.should be_success
+    describe "by anonymous user" do
+      before{ get 'index' }
+      it{ response.should be_success }
+      it{ response.should render_template("index") }
+    end
+    describe "by user" do
+      before do
+        login_as(@user)
+        get 'index'
+      end
+      it{ response.should be_success }
+      it{ response.should render_template("index") }
+    end
+  end
+
+  describe "GET new" do
+    describe "by anonymous user" do
+      before{ get 'new' }
+      it{ response.should be_redirect }
+    end
+    describe "by user" do
+      before do
+        login_as(@user)
+        get 'new'
+      end
+      it{ response.should be_success }
+      it{ response.should render_template("new") }
+    end
+  end
+
+  describe "GET show" do
+    describe "by anonymous user" do
+      before{ get 'show' }
+      it{ response.should be_redirect }
+    end
+    describe "by user" do
+      before do
+        login_as(@user)
+        get 'show'
+      end
+      it{ response.should be_success }
+      it{ response.should render_template("show") }
+    end
+  end
+
+  describe "GET edit" do
+    describe "by anonymous user" do
+      before{ get 'edit' }
+      it{ response.should be_redirect }
+    end
+    describe "by user" do
+      before do
+        login_as(@user)
+        get 'edit'
+      end
+      it{ response.should be_success }
+      it{ response.should render_template("edit") }
+    end
+  end
+
+  describe "POST create" do
+    describe "by anonymous user" do
+      before{ post 'create' }
+      it{ response.should be_redirect }
+    end
+
+  end
+
+  describe "PUT update" do
+    describe "by anonymous user" do
+      before{ put 'update' }
+      it{ response.should be_redirect }
+    end
+  end
+
+  describe "DELETE destroy" do
+    describe "by anonymous user" do
+      before{ delete 'destroy' }
+      it{ response.should be_redirect }
     end
   end
 end

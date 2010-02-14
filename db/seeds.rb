e6ab69ea37f108d7f5cb94fc5314d1c7b85452d3
okkez <okkez000@gmail.com>
2010-02-14 21:15:02 +0900

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,8 +46,7 @@ ActiveRecord::Base.transaction do
     item2.save!
   end
 
-  root = Keyword.create!(:name => "root")
-  cost = root.children.create!(:name => "費用")
+  cost = Keyword.create!(:name => "費用")
   [
    "その他", "オンラインサービス", "クリーニング", "ケーブルテレビ",
    "コンピュータ", "プロバイダ", "委託手数料", "衣料品", "医療費",
@@ -59,11 +58,10 @@ ActiveRecord::Base.transaction do
    ["税金", ["その他の税", "健康保険", "公的年金", "国税", "地方税"]],
    "贈答", "調整", "電話料金", "日用品", ["勉強会", ["懇親会", "参加費"]],
    ["保険", ["賃貸住宅保険", "医療保険", "自動車保険", "生命保険"]],
-   ["負債", ["クレジットカード"]]
   ].each do |w|
     add_child_keyword(cost, w)
   end
-  assets = root.children.create!(:name => "資産")
+  assets = Keyword.create!(:name => "資産")
   [
    ["投資", ["委託売買口座",
              ["オープンエンド型投資信託", "マーケット指標", "株式", "債権"]]],
@@ -71,18 +69,24 @@ ActiveRecord::Base.transaction do
   ].each do |w|
     add_child_keyword(assets, w)
   end
-  earnings = root.children.create!(:name => "収益")
+  earnings = Keyword.create!(:name => "収益")
   [
    "その他の収入", "ボーナス", "給与", "贈与収入",
    ["利息収入", ["その他の利息", "当座預金利息", "普通預金利息"]]
   ].each do |w|
     add_child_keyword(earnings, w)
   end
-  net_assets = root.children.create!(:name => "純資産")
+  net_assets = Keyword.create!(:name => "純資産")
   [
    "開始残高"
   ].each do |w|
     add_child_keyword(net_assets, w)
+  end
+  debt = Keyword.create!(:name => "負債")
+  [
+   "クレジットカード"
+  ].each do |w|
+    add_child_keyword(debt, w)
   end
 end
 

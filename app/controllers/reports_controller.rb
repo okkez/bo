003d5d320d7c36f8527a253157ecc290b2517dcd
okkez <okkez000@gmail.com>
2010-02-20 22:35:48 +0900

    implement reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,27 @@
+# -*- coding: utf-8 -*-
 class ReportsController < ApplicationController
+
+  before_filter :authentication_required
+
   def index
   end
 
   def search
+    first = Date.parse(params[:first])
+    last  = Date.parse(params[:last])
+    @events = current_user.events.by_range(first, last).all(:include => { :items => :taggings })
+    @keywords = Keyword.roots.map{|root| root.self_and_descendants }.flatten.map(&:name)
+    @report = @events.each_with_object(Hash.new{|h,k| h[k] = Hash.new(0) }){|event, memo|
+      event.items.each{|item|
+        item.tag_list.each{|tag|
+          memo[tag][:founds_in] += item.founds_in || 0
+          memo[tag][:founds_out] += item.founds_out || 0
+        }
+      }
+    }
+  end
+
+  def range
   end
 
 end
--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -2,23 +2,11 @@ require 'spec_helper'
 
 describe ReportsController do
 
-  #Delete these examples and add some real ones
-  it "should use ReportsController" do
-    controller.should be_an_instance_of(ReportsController)
-  end
 
 
   describe "GET 'index'" do
-    it "should be successful" do
-      get 'index'
-      response.should be_success
-    end
   end
 
   describe "GET 'search'" do
-    it "should be successful" do
-      get 'search'
-      response.should be_success
-    end
   end
 end

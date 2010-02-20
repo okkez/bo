6138e74764c99346de645086dca65387b911675a
okkez <okkez000@gmail.com>
2010-02-20 21:34:29 +0900

    add default value

--- db/migrate/20091101132031_create_items.rb
+++ db/migrate/20091101132031_create_items.rb
@@ -2,8 +2,8 @@ class CreateItems < ActiveRecord::Migration
   def self.up
     create_table :items do |t|
       t.integer :event_id
-      t.integer :founds_in
-      t.integer :founds_out
+      t.integer :founds_in, :null => false, :default => 0
+      t.integer :founds_out, :null => false, :default => 0
       t.timestamps
       t.integer :lock_version, :null => false, :default => 0
     end

d2a03ed2eef3e07c533b823d8714f760e96502ad
okkez <okkez000@gmail.com>
2010-05-04 15:17:13 +0900

    add events/new view spec

---
+++ spec/views/events/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe "/events/new" do
+  describe "with template" do
+    before do
+      assigns[:event] = Factory.build(:event,
+                                      :user => Factory(:user),
+                                      :items => [Factory.build(:item),
+                                                 Factory.build(:item, :founds_in => 333)])
+      render 'events/new'
+    end
+    it{ response.should have_tag('table.event') }
+    it do
+      response.should have_tag('table.items') do
+        with_tag("tbody tr td.founds-in input[value=?]", '333')
+      end
+    end
+    it{ response.should have_tag("table.event tbody tr th", "テンプレート") }
+  end
+end
